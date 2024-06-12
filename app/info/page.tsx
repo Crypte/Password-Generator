0b97d6ff4eef10effc1779a8f232bbce9e4627cb
louisM <louisconnect@pm.me>
2024-06-12 13:10:38 +0200

    Docs

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,17 +1,11 @@
 import { Faq } from "@/components/Faq";
 import { H1 } from "@/components/H1";
 import { H2 } from "@/components/H2";
-import { H3 } from "@/components/H3";
 import { List } from "@/components/List";
 import { P } from "@/components/P";
 import { Table } from "@/components/Table";
 
 export default function Home() {
-  const exampleData = [
-    ["Generator", "hash algorithm", "Response encoding", "Method"],
-    ["Password", "SHA-512", "Base64", "First 20 caracters"],
-    ["Pin", "SHA-512", "Hex", "First X numbers"],
-  ];
   const security = [
     "Generator",
     "hash algorithm",
@@ -19,13 +13,9 @@ export default function Home() {
     "Method",
   ];
 
-  const privacy = [
-    "The app run locally on your browser",
-    "No data are stored and send",
-  ];
   return (
     <div className="md:max-w-3xl container mt-24">
-      <H1>WIP</H1>
+      <H1>Hello Users,</H1>
       <H2>Introduction</H2>
       <P>
         This tool is designed to give you a simple, practical and easily
@@ -38,15 +28,28 @@ export default function Home() {
         The generation mainly use SHA512, an hash algorithm that allow us to
         generate for a same entry always the same exit.
       </P>
-      <List data={security} />
-      <Table data={exampleData} />
+      <List
+        data={["Generator", "hash algorithm", "Response encoding", "Method"]}
+      />
+      <Table
+        data={[
+          ["Generator", "hash algorithm", "Response encoding", "Method"],
+          ["Password", "SHA-512", "Base64", "First 20 caracters"],
+          ["Pin", "SHA-512", "Hex", "First X numbers"],
+        ]}
+      />
       <H2>Privacy</H2>
       <P>
         The app run locally on your browser, no data are stored and send. You
         can check the source code on Github
       </P>
-      <List data={privacy} />
-      <H3>Step 1</H3>
+      <List
+        data={[
+          "The app run locally on your browser",
+          "No data are stored or send",
+        ]}
+      />
+
       <H2>Faq</H2>
       <Faq />
     </div>
